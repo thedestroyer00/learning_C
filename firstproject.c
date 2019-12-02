0b0705f15c115fc9c8502f9a54c7ea98bbf2e57a
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2019-12-02 20:18:18 +0545

    Update firstproject.c

--- firstproject.c
+++ firstproject.c
@@ -6,35 +6,15 @@
 
 int main()
 {
-    int givendays;
-    printf("enter the given days : ");
-    scanf("%d", &givendays);
-    int years,remain_days, months, days;
-
-    if(givendays > 365){
-
-        years = givendays / 365;
-        printf("%d years ", years);
-        remain_days = givendays % 365;
-        //printf("%d", remain_days);
-        months = remain_days / 30;
-        printf("%d months ", months);
-        days = remain_days % 30;
-        printf("%d days ", days);
-
-    }
-    else if(30 <= givendays < 365){
-
-        printf("0 years ");
-        remain_days = givendays;
-        months = remain_days / 30;
-        printf("%d months ", months);
-        days = remain_days % 30;
-        printf("%d days", days);
-
-    }
-   else if( givendays < 30){
-    printf("0 years 0 months %d days ", givendays);
-   }
-       return 0;
-    }
+    int days, years, months, day, remain_day;
+    printf("enter the number of days : ");
+    scanf("%d", &days);
+    
+    years = days/ 365;
+    remain_day = days%365;
+    months = remain_day/30;
+    day = remain_day%30;
+    
+    printf("%d years %d months %d days", years, months, day);
+    return 0;
+}
