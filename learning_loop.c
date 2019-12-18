8cc7341be595e55c6fdbf8f983f217e080683483
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2019-12-18 18:39:44 +0545

    Update learning_loop.c

--- learning_loop.c
+++ learning_loop.c
@@ -7,26 +7,28 @@
 		       ***********
 */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
 	int i, j, k;
-	for(i=1; i <= 5; i++)
-	{
-		for(j = 5-i; j > 0; j--)
-		{
-			printf(" ");
-		}
-		for(k = 1; j <= 2*i-1; k++)
-		{
-			printf("* ");
-		}
-		printf("\n");
-	}
+
+	for(i=1; i<= 5; i++)
+    {
+        for(j = 5-i; j>0; j--)
+        {
+            printf(" ");
+        }
+        for(k=1; k <= 2*i-1; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
 	return 0;
-	
+
 }
-	
+
 			
