6cd7525231f522054fb98645411a8f0167253491
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2019-12-18 18:32:37 +0545

    Update learning_loop.c

--- learning_loop.c
+++ learning_loop.c
@@ -1,8 +1,32 @@
 // program to print following patter :
-/*              *
+/*                          *
 			   ***
 			  *****
 			 *******
 			*********
-		   ***********
+		       ***********
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i, j, k;
+	for(i=1; i <= 5; i++)
+	{
+		for(j = 5-i; j > 0; j--)
+		{
+			printf(" ");
+		}
+		for(k = 1; j <= 2*i-1; k++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+	
+}
+	
+			
