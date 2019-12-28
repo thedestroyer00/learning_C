549fe08d73d715e1faa555d80dc871ebe7dfd8ce
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2019-12-28 17:28:29 +0545

    Create firstproject.c

---
+++ learning/firstproject.c
@@ -0,0 +1,21 @@
+
+//Program to convert give days into years-months-days format
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int days, years, months, day, remain_day;
+    printf("enter the number of days : ");
+    scanf("%d", &days);
+    
+    years = days/ 365;
+    remain_day = days%365;
+    months = remain_day/30;
+    day = remain_day%30;
+    
+    printf("%d years %d months %d days", years, months, day);
+    return 0;
+}
