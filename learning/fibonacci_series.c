2afb690f7d95568d40b8b98a275fc1b33972ea21
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2019-12-28 17:29:17 +0545

    Create fibonacci_series.c

---
+++ learning/fibonacci_series.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 0, b = 1, lim;
+	printf("enter the limit of the series : ");
+	scanf("%d", &lim);
+	
+	while( a <= lim || b <= lim)
+	{
+		printf("%d %d ", a,b);
+		a = a + b;
+		b = b + a;
+	}
+	
+	return 0;
+	
+}
