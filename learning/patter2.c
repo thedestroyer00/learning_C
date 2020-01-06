312ed2afac34aaad0740dd7730abe68020818a96
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-01-06 18:29:14 +0545

    Create patter2.c

---
+++ learning/patter2.c
@@ -0,0 +1,32 @@
+//program to print the following pattern: 
+/*
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    int len = n*2 - 1,min;
+    for(int i=0;i<len;i++){
+        for(int j=0;j<len;j++){
+            min = i < j ? i : j;
+            min = min < len-i ? min : len-i-1;
+            min = min < len-j-1 ? min : len-j-1;
+            printf("%d ", n-min);
+        }
+        printf("\n");
+    }
+    return 0;
+}
