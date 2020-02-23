929be109bbba0e0d21702347904593dadfe608bc
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-23 20:47:36 +0545

    Create pattern2.c

---
+++ homework/pattern2.c
@@ -0,0 +1,29 @@
+// To print the following pattern: 
+/*
+		*
+	   * *
+	  * * *
+	 * * * *
+*/
+
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+{
+	int i,j,k;
+	for(i = 1; i<5; i++)
+	{
+		for(k = 4-i; k>0; k--)
+		{
+			printf(" ");	
+		}
+		for(j =1; j<= i; j++)
+		{
+			int l = j;
+			printf(" %c",'*');
+		}
+				
+		printf("\n");	
+	}
+}
